59d573e8cde559ea1959f34ba7e0cca5c07d7d06
lukeskywlker19 <thelukeskywlker19@gmail.com>
2026-03-05 17:47:11 -0500

    Update the gradle wrapper. Update the quickshop api to 6.2.0.11. Change the plugin version format from X.X.X to X.X.X.X.
    Signed-off-by: lukeskywlker19 <thelukeskywlker19@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.github.lukesky19"
-version = "1.0.1"
+version = "1.0.1.0"
 
 repositories {
     mavenCentral()
@@ -25,11 +25,9 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-    //compileOnly("com.ghostchu:quickshop-bukkit:6.2.0.6")
-    compileOnly("com.ghostchu:quickshop-api:6.2.0.8")
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("com.ghostchu:quickshop-api:6.2.0.11")
     compileOnly("fr.neatmonster:nocheatplus:3.16.1-SNAPSHOT")
-    //implementation("org.spongepowered:configurate-yaml:4.1.2")
 }
 
 java {
